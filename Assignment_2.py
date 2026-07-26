e6f8e85bb871cf4d05713e4deec18f58ed9fd5c2
Sarthak <sagaik95@gmail.com>
2026-07-26 21:23:15 +0530

    Add files via upload

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,1 +1,53 @@
+# Decorator
+def message(func):
+    def show(self):
+        print("Report is Ready")
+        func(self)
+    return show
 
+
+class Report:
+
+    template = "Simple Report"
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    @classmethod
+    def change_template(cls, new_template):
+        cls.template = new_template
+
+    def __str__(self):
+        return "Title : " + self.title + "\nContent : " + self.content
+
+    @message
+    def display(self):
+        print("Template :", Report.template)
+        print(self)
+
+
+# Main Program
+r1 = Report("Student Report", "Marks are good.")
+
+print("Before Changing Template")
+r1.display()
+
+Report.change_template("College Report")
+
+print("\nAfter Changing Template")
+r1.display()
+
+#output
+# Before Changing Template
+# Report is Ready
+# Template : Simple Report
+# Title : Student Report
+# Content : Marks are good.
+
+# After Changing Template
+# Report is Ready
+# Template : College Report
+# Title : Student Report
+# Content : Marks are good.
+# sarthak@192 OOPs % 
